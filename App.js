e4d1f98866d1d0461b8d6449131758a1222df39e
Chacko <chackop@gmail.com>
2020-02-14 12:05:22 +0000

    added libs and started

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, Image } from 'react-native';
 
 export default function App() {
 
+  const [currentScreen, setCurrentScreen] = useState("search");
+
+  const switchScreen = (currentScreen) => {
+    setCurrentScreen(currentScreen)
+  }
+
+  const renderScreen = () => {
+    if (currentScreen === "landing") {
+      return (
+        <Landing switchScreen={switchScreen} />
+      )
+    }
+    else if (currentScreen === "search") {
+      return (
+        <Search />
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {renderScreen()}
     </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    marginTop: Platform.OS === "android" ? 24 : 0
+  }
 });
